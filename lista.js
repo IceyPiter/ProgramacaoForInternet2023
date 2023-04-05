e97cf2cdd5ca967dd2fcdd5f6fdc177c860c38b0
IceyPiter <124799280+IceyPiter@users.noreply.github.com>
2023-04-05 11:58:43 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -1,8 +1,12 @@
 const nameList = ["Aline","Otavio","Camila","Nareba","Jean", "Julio", "Julio Rodalo", "Rany", "Ti"]
 
 const listEl = document.getElementById("list");
-const searchField = document.getElementById("searchField");
-searchField.addEventListener("input", inputHandler);
+const addButton = document.getElementById("buttonAdd")
+const removeButton = document.getElementById("buttonRemove");
+const selectButton = document.getElementById("buttonSelect");
+const filtButton = document.getElementById("buttonFilt");
+filtButton.addEventListener("click",filtHandler)
+addButton.addEventListener("click",addList)
 
 this.fillList();
 
@@ -15,11 +19,25 @@ function fillList(list = nameList) {
     }
 }
 
-function inputHandler(){
+function addList() {
+    nameList.push(searchField.value);
+    fillList();
+    }
+
+function selectName() {
+    
+}
+
+
+function filtHandler(){
     const filteredList = nameList.filter(el => {
         const listItems = el.toLowerCase();
         const searchWorld = searchField.value.toLowerCase();
         return listItems.includes(searchWorld)
     });
     fillList(filteredList)
+}
+
+function addHandler(){
+
 }
