9385c3822504fe844ac705290ea95d5a34d14af4
Piterzin <124799280+IceyPiter@users.noreply.github.com>
2023-06-15 15:42:22 -0300

    Add files via upload

--- ExerciciosWeb/indexP.js
+++ ExerciciosWeb/indexP.js
@@ -1,10 +1,67 @@
 function calculo(){
-    const valor = Number(document.getElementById("valord").value)
-    const resp = document.getElementById("resposta")
-    if(valor < 1){
-        resp.innerText = "Valor Insuficiente"
-    }
-    if(valor >= 1 && valor < 1.75){
-        
-    }
+    document.querySelector("form").addEventListener("submit", (e)=> {
+        e.preventDefault()
+        const valor = Number(document.getElementById("valor").value)
+        const resp = document.getElementById("resposta")
+        const trocos = document.getElementById("troco")
+        let tr = 0
+        let tp = 0
+        console.log(valor)
+        if(valor < 1){
+          resp.innerText = "Valor Insuficiente"
+          trocos.innerText = "Seu troco é: " + valor
+        }
+        else if(valor < 1.75){
+            tp = 30
+            resp.innerText = `Você tem ${tp} minutos...`
+            if(valor != 1){
+                tr = valor - 1
+                trocos.innerText = "Seu troco é: " + tr 
+            }else{
+                trocos.innerText = "Seu troco é: 0"
+            }
+        }
+        else if(valor < 3){
+            tr = valor - 3
+            if(tr >= 1.75){
+                tr = tr - 1.75
+                tp = tp + 60
+            }
+            if(tr >= 1){
+                tr = tr - 1
+                tp = tp + 30
+            }
+            resp.innerText = `Você tem ${tp} minutos...`
+            if(valor != 1){
+                tr = valor - 1.75
+                trocos.innerText = "Seu troco é: " + tr 
+            }
+        }
+        else if(valor >= 3){
+            if(valor > 3){
+                tp = (valor-valor%3)/3
+                tr = valor - 3*tp
+                tp = tp*120
+                if(tr >= 1.75){
+                    tr = tr - 1.75
+                    tp = tp + 60
+                }
+                if(tr >= 1){
+                    tr = tr - 1
+                    tp = tp + 30
+                }
+            }
+            else{
+                tr = valor - 3
+                tp = "120"
+            }
+            resp.innerText = `Você tem ${tp} minutos...`
+            if(valor != 3){
+                trocos.innerText = "Seu troco é: " + tr 
+            }
+            else{
+                trocos.innerText = "Seu troco é: 0"
+            }
+        }
+    })
 }
--- ExerciciosWeb/indexT.js
+++ ExerciciosWeb/indexT.js
@@ -3,13 +3,17 @@ function triangulo(){
     const elem2 = Number(document.getElementById("lado2").value);
     const elem3 = Number(document.getElementById("lado3").value);
     const resp = document.getElementById("resposta");
-    if(elem1 == elem2 && elem1 == elem3){
-        resp.innerText = "Triângulo Equilátero"
-    }
-    else if(elem1 == elem2 || elem1 == elem3 || elem2 == elem3){
-        resp.innerText = "Triâgulo Isóceles"
-    }
-    else if(elem1 != elem2 && elem1 != elem3 && elem2 != elem3){
-        resp.innerText = "Triângulo Escaleno"
-    }
+
+    document.querySelector("form").addEventListener("submit", (e)=> {
+        e.preventDefault()
+        if(elem1 == elem2 && elem1 == elem3){
+            resp.innerText = "Triângulo Equilátero"
+        }
+        else if(elem1 == elem2 || elem1 == elem3 || elem2 == elem3){
+            resp.innerText = "Triâgulo Isóceles"
+        }
+        else if(elem1 != elem2 && elem1 != elem3 && elem2 != elem3){
+            resp.innerText = "Triângulo Escaleno"
+        }
+    })
 }
