1d462e05a2df40f3147f737d8ba3dcc5ec3cc841
Piterzin <124799280+IceyPiter@users.noreply.github.com>
2023-06-15 13:27:01 -0300

    Add files via upload

---
+++ ExerciciosWeb/indexP.js
@@ -0,0 +1,10 @@
+function calculo(){
+    const valor = Number(document.getElementById("valord").value)
+    const resp = document.getElementById("resposta")
+    if(valor < 1){
+        resp.innerText = "Valor Insuficiente"
+    }
+    if(valor >= 1 && valor < 1.75){
+        
+    }
+}
---
+++ ExerciciosWeb/indexT.js
@@ -0,0 +1,15 @@
+function triangulo(){
+    const elem1 = Number(document.getElementById("lado1").value);
+    const elem2 = Number(document.getElementById("lado2").value);
+    const elem3 = Number(document.getElementById("lado3").value);
+    const resp = document.getElementById("resposta");
+    if(elem1 == elem2 && elem1 == elem3){
+        resp.innerText = "Triângulo Equilátero"
+    }
+    else if(elem1 == elem2 || elem1 == elem3 || elem2 == elem3){
+        resp.innerText = "Triâgulo Isóceles"
+    }
+    else if(elem1 != elem2 && elem1 != elem3 && elem2 != elem3){
+        resp.innerText = "Triângulo Escaleno"
+    }
+}
