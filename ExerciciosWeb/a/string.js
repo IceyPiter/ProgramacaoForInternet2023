3159d3d91ec4ae74d1e67b24c7a4043368ca0baf
Piterzin <124799280+IceyPiter@users.noreply.github.com>
2023-07-05 22:06:28 -0300

    Add files via upload

--- ExerciciosWeb/a/string.js
+++ ExerciciosWeb/a/string.js
@@ -6,21 +6,20 @@ frm.addEventListener("submit", (e) => {
     let text = frm.valor1.value;
     let insert = frm.valor2.value;
     let posicions = frm.valor3.value;
-    let vetor = text.split("")
     let pos = posicions.split(";")
     let resp = []
-
-    for(let i = 0; i< vetor[0].length;i++){
-        resp.push(vetor[0][i])
+    for(let i = 0; i< text.length;i++){
+        resp.push(text[i])
     }
-    for(let x = 0; x < posicions.length;x++){
-        if(vetor[x] == "0"){
-            resp.splice(text[x],0,insert + " ")
+    console.log(resp)
+    for(let x = 0; x < pos.length;x++){
+        if(pos[x] == "0"){
+            resp.splice(pos[parseInt(x)],0,insert + " ")
         }
-        else if(vetor[x] == vetor[1]){
-            resp.splice(text[x],0," " + insert)
+        else if(parseInt(pos[x]) == text.length){
+            resp.splice(pos[parseInt(x)],0," " + insert)
         }else{
-            resp.splice(text[x],0," " + insert)
+            resp.splice(pos[parseInt(x)],0," " + insert)
         }
     }
     saida.innerText = resp.join("")
